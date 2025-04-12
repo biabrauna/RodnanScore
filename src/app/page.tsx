a7b2d3d761adb42fffc0216ad1b5c8ca74a952f0
biabrauna <biabn443@gmail.com>
2025-04-12 15:50:49 -0300

    refactor: my name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -223,7 +223,7 @@ export default function AvaliacaoPage() {
             Salvar Avaliação
           </Button>
         </div>
-      <p className="text-md text-center mt-5 md:text-sm text-black dark:text-black">Idealizado por José Vicente</p>
+      <p className="text-md text-center mt-5 md:text-sm text-black dark:text-black">Idealizado por José Vicente e<br></br>desenvolvido por Bia Braúna</p>
       </div>
     </div>
   )
